dae339cef4ff2cd392c765427404a619f561230e
INI\lewis.nguyen <lewis.nguyen@terralogic.com>
2020-07-21 16:32:29 +0700

    move error msg to above

--- src/pages/Login/LoginForm/index.js
+++ src/pages/Login/LoginForm/index.js
@@ -33,7 +33,6 @@ export default class LoginForm extends Component {
     // VALIDATE EMAIL
     validate = (email) => {
         const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-
         return expression.test(String(email).toLowerCase())
     }
 
@@ -43,7 +42,7 @@ export default class LoginForm extends Component {
         let errorMsg = '';
         let isError = false;
         if (this.state.email === '' && this.state.password === '') {
-            errorMsg = 'Please fill the inputs';
+            errorMsg = 'Please type your email and password to login';
             isError = true;
         }
         else if (this.state.email === '' && this.state.password !== '') {
@@ -85,6 +84,14 @@ export default class LoginForm extends Component {
         return (
             <div className="login-form">
                 <h2 className="form-title">Login Your Account</h2>
+
+                {/* ERROR MESSAGE */}
+                {this.state.isError ?
+                    <div className="alert alert-danger alert-dismissible">
+                        <strong>{this.state.error}</strong>
+                    </div> : <div></div>
+                }
+                
                 <form className="form-main" onSubmit={this.handleSubmit}>
                     <RoundedInput
                         title="Email"
@@ -127,13 +134,6 @@ export default class LoginForm extends Component {
                         onChange={this.rememberPasswordOnChange}
                     />
                 </form>
-
-                {/* ERROR MESSAGE */}
-                {this.state.isError ?
-                    <div className="alert alert-danger alert-dismissible">
-                        <strong>{this.state.error}</strong>
-                    </div> : <div></div>
-                }
             </div>
         )
     }
