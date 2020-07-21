e5914376aad941efa1f378aca39b701bfc339a14
INI\lewis.nguyen <lewis.nguyen@terralogic.com>
2020-07-21 10:41:21 +0700

    update validate email and password, add error message

--- src/pages/Login/LoginForm/index.js
+++ src/pages/Login/LoginForm/index.js
@@ -6,16 +6,61 @@ import ShowPasswordIcon from '../../../assets/images/Suche03.svg';
 import Button from '../../../components/Button';
 
 export default class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            isError: false,
+            errorMsg: '',
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    // validate email
+    validate = (email) => {
+        const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+
+        return expression.test(String(email).toLowerCase())
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        if (this.state.email === '' || this.state.password === '') {
+            this.setState({ error: 'Please fill the inputs', isError: true });
+        }
+        else if (!this.validate(this.state.email)) {
+            this.setState({ error: `${this.state.email} is not an email`, isError: true });
+        }
+        else if (this.state.password.length < 6) {
+            this.setState({ error:  'Password must be at least 6 characters', isError: true });
+        }
+        else {
+            this.setState({ error: '', isError: false });
+        }
+    }
+
     render() {
         return (
             <div className="login-form">
-                <h2 className="form-title">Login Your Account</h2>    
-                <form>
+                <h2 className="form-title">Login Your Account</h2>
+                <form className="form-main" onSubmit={this.handleSubmit}>
                     <div className="input-container">
                         <p className="input-title">Email</p>
                         <div className="input-with-icon">
                             <img className="input-icon" src={EmailIcon} />
-                            <input className="input-text" type="text" placeholder="Enter your email" name="email" />
+                            <input
+                                className="input-text"
+                                type="text"
+                                placeholder="Enter your email"
+                                name="email"
+                                onChange={this.handleChange}
+                            />
                         </div>
                     </div>
 
@@ -23,7 +68,13 @@ export default class LoginForm extends Component {
                         <p className="input-title">Password</p>
                         <div className="input-with-icon">
                             <img className="input-icon" src={PasswordIcon} />
-                            <input className="input-text" type="password" placeholder="Enter your password" name="password" />
+                            <input
+                                className="input-text"
+                                type="password"
+                                placeholder="Enter your password"
+                                name="password"
+                                onChange={this.handleChange}
+                            />
                             <img className="show-password-icon" src={ShowPasswordIcon} />
                         </div>
                     </div>
@@ -32,13 +83,21 @@ export default class LoginForm extends Component {
                         <Button buttonType="button" buttonClassName="button-type-2" buttonValue="Register" />
                         <Button buttonType="submit" buttonClassName="button-type-1" buttonValue="Login" />
                     </div>
-                    
+
 
                     <div className="remember-login">
                         <input type="checkbox" name="remember-pwd" />
                         Remember password
                     </div>
                 </form>
+
+                {/* ERROR MESSAGE */}
+                {this.state.isError ?
+                    <div className="alert alert-danger alert-dismissible">
+                        <button type="button" className="close" data-dismiss="alert">&times;</button>
+                        <strong>{this.state.error}</strong>
+                    </div> : <div></div>
+                }
             </div>
         )
     }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -8,19 +8,22 @@ export default class Login extends Component {
     render() {
         return (
             <div>
-                <div className="container login-container p-5">
-                    <div className="row align-items-center">
-                        <div className="col-md login-form-container">
-                            <CompanyLogo />
-                            <LoginForm />
+                <div className="container mt-md-5 mb-5">
+                    <div className="row">
+                        <div className="col-sm-12 col-md">
+                            <div className="container-fluid">
+                                <div className="row">
+                                    <div className="col-md-11">
+                                        <CompanyLogo />
+                                        <LoginForm />
+                                    </div>
+                                    <div className="col-md-1 d-none d-md-block">
+                                        {/* Empty col */}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div></div>
-                    </div>
-                </div>
-                <div class="container-fluid abs">
-                    <div class="row align-items-center">
-                        <div class="col-md"></div>
-                        <div class="col-md login-picture-container d-none d-lg-block">
+                        <div className="col-sm-0 col-md d-none d-md-block pl-5 align-self-center">
                             <CompanyImage />
                         </div>
                     </div>
