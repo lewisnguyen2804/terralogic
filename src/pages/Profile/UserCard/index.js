f8ac6956c21092bd78af51ae574830cc58d8643f
INI\lewis.nguyen <lewis.nguyen@terralogic.com>
2020-07-22 14:03:21 +0700

    add change user image icon

--- src/pages/Profile/UserCard/index.js
+++ src/pages/Profile/UserCard/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import './style.scss';
+import ChangeUserImage from '../../../assets/images/edit_photo.svg';
 
 export default class UserCard extends Component {
     render() {
         return (
             <div className="name-image">
-                <img className="user-image" alt="user-img" src={this.props.image} />
+                <div className="user-image-change">
+                    <img className="user-image" alt="user-img" src={this.props.image} />
+                    <img className="user-image-change-icon" src={ChangeUserImage} alt="change-user-img" />
+                </div>
                 <h2 className="user-name">{this.props.name}</h2>
             </div>
         )
