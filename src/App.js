73e640afa8c1e421aa1517bd6884413c8998fea2
INI\lewis.nguyen <lewis.nguyen@terralogic.com>
2020-07-29 11:10:05 +0700

    add alert's close button

--- src/App.js
+++ src/App.js
@@ -15,41 +15,55 @@ import ProfileContainer from './pages/Profile';
 
 import { history } from './helpers';
 import { connect } from 'react-redux';
+import { alertActions } from './actions';
+import { bindActionCreators } from 'redux';
 
 class App extends Component {
 	render() {
 		const { alert } = this.props;
-		const alertType = alert.type === 'alert-success' ? 'bg-success':'bg-danger';
+		const alertType = alert.type === 'alert-success' ? 'bg-success' : 'bg-danger';
 		return (
 			<div>
 				{alert.message &&
-					<div className={`text-center text-white p-2 fixed-top ${alertType}`}>
+					<div className={`alert-in-app text-center fade show text-white p-2 fixed-top ${alertType}`}>
 						{alert.message}
+						<div className="alert-dissmiss-btn">
+							<button
+								type="button"
+								onClick={() => { this.props.alertClear() }}
+								class="close"
+								data-dismiss="alert">&times;</button>
+						</div>
 					</div>
 				}
 
 				<Router history={history}>
-					<div>
-						<Switch>
-							<PrivateRoute exact path="/" component={ProfileContainer} />
-							<Route path="/login">
-								<Login />
-							</Route>
-							<Route path="/register">
-								<Register />
-							</Route>
-						</Switch>
+							<div>
+								<Switch>
+									<PrivateRoute exact path="/" component={ProfileContainer} />
+									<Route path="/login">
+										<Login />
+									</Route>
+									<Route path="/register">
+										<Register />
+									</Route>
+								</Switch>
+							</div>
+						</Router>
 					</div>
-				</Router>
-			</div>
 		);
 	}
 }
 
 let mapStateToProps = (state) => {
-	const { alert } = state;
-	return { alert };
+	const { alert} = state;
+	return { alert};
+}
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({
+					alertClear: alertActions.clear
+	}, dispatch)
 }
 
-const ConnectedApp = connect(mapStateToProps)(App);
+const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App);
 export default ConnectedApp;
